1d40cb61d85e803955f0e490c65e27bf09479dfe
adriandamii <37124920+adriandamii@users.noreply.github.com>
2022-07-05 19:44:12 +0300

    Update Translation.js

--- client/src/components/Translation.js
+++ client/src/components/Translation.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { ListGroup } from 'react-bootstrap';
+import { Badge, ListGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Translation = (props) => {
   const { translation } = props;
   return (
     <div>
-      <ListGroup.Item>
-        <Link to={`/${translation._id}`}>{translation.name}</Link>
-      </ListGroup.Item>
+      <Link to={`/${translation._id}`}>
+        <ListGroup.Item action variant="warning" className="d-flex justify-content-between align-items-start">
+        {translation.name}
+        <Badge>
+          {translation.numberOfPages} pages
+        </Badge>
+        </ListGroup.Item>
+      </Link>
     </div>
   );
 };
