2d93aea02d1e6145f7e8ac17c243409fa1f4849d
adriandamii <37124920+adriandamii@users.noreply.github.com>
2022-07-05 19:48:42 +0300

    SearchBox component added

---
+++ client/src/components/SearchBox.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import { useNavigate } from 'react-router-dom';
+
+export default function SearchBox() {
+  const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate(`/search/name/${name}`);
+  };
+
+  return (
+    <Form className="d-flex me-auto" onSubmit={submitHandler}>
+      <InputGroup>
+        <FormControl
+          type="text"
+          name="name"
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="search translations..."
+          aria-label="Search Translations"
+          aria-describedby="button-search"
+        ></FormControl>
+        <Button variant="outline-primary" type="submit" id="button-search">
+          <i className="fas fa-search"></i>
+        </Button>
+      </InputGroup>
+    </Form>
+  );
+}
