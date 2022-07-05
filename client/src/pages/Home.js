94c374edb269af339ff1dbcaf7a0887cad27b596
adriandamii <37124920+adriandamii@users.noreply.github.com>
2022-07-05 20:03:03 +0300

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getTranslations } from '../actions/translations';
+import { getTranslations, getOffices } from '../actions/translations';
 import { Helmet } from 'react-helmet-async';
 import Translation from '../components/Translation';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { Col, Row } from 'react-bootstrap';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -12,7 +13,8 @@ const Home = () => {
   const { loading, error, translations } = translationsList;
 
   useEffect(() => {
-    dispatch(getTranslations());
+    dispatch(getTranslations({}));
+    dispatch(getOffices());
   }, [dispatch]);
 
   return (
@@ -26,12 +28,16 @@ const Home = () => {
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
         <>
-          {translations.length === 0 && (
-            <MessageBox>No Translation Found</MessageBox>
-          )}
-          {translations.map((translation) => (
-            <Translation key={translation._id} translation={translation} />
-          ))}
+          <Row>
+            <Col md={{ span: 8, offset: 2 }}>
+              {translations.length === 0 && (
+                <MessageBox>No Translation Found</MessageBox>
+              )}
+              {translations.map((translation) => (
+                <Translation key={translation._id} translation={translation} />
+              ))}
+            </Col>
+          </Row>
         </>
       )}
     </div>
