0bc519dbe343251e7a847b733aff6fa4b1647856
adriandamii <37124920+adriandamii@users.noreply.github.com>
2022-07-18 18:52:03 +0300

    Added a boolean for translation

--- server/models/translation.js
+++ server/models/translation.js
@@ -1,11 +1,17 @@
 import mongoose from 'mongoose';
 
-const translationSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  office: { type: String, required: true },
-  numberOfPages: { type: String, required: true },
-  pagePrice: { type: String, required: true },
-});
+const translationSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    office: { type: String, required: true },
+    numberOfPages: { type: String, required: true },
+    pagePrice: { type: String, required: true },
+    translated: { type: Boolean, default: 'false' },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 var Translation = mongoose.model('Translation', translationSchema);
 
