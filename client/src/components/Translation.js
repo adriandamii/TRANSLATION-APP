af9894a0635ea6e74b3a89cafb0387721880667f
adriandamii <37124920+adriandamii@users.noreply.github.com>
2022-07-18 19:26:44 +0300

    Added checkbox

--- client/src/components/Translation.js
+++ client/src/components/Translation.js
@@ -1,19 +1,47 @@
 import React from 'react';
-import { Badge, ListGroup } from 'react-bootstrap';
+import { Badge, Form, ListGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Translation = (props) => {
-  const { translation } = props;
+  const { translation, setTranslations } = props;
+  
+  const completeTranslation = async (id) => {
+    const data = await fetch(
+      `http://localhost:5000/translations/complete/${id}`
+    ).then((res) => res.json());
+    
+    setTranslations((translations) =>
+      translations.map((tr) => {
+        if (tr._id === data._id) {
+          tr.translated = data.translated;
+        }
+        return tr;
+      })
+    );
+  };
+  
   return (
     <div>
-      <Link to={`/${translation._id}`}>
-        <ListGroup.Item action variant="warning" className="d-flex justify-content-between align-items-start">
-        {translation.name}
-        <Badge>
-          {translation.numberOfPages} pages
-        </Badge>
-        </ListGroup.Item>
-      </Link>
+      <ListGroup.Item
+        key={translation._id}
+        action
+        variant="warning"
+        className="d-flex justify-content-between"
+      >
+        <div className="d-flex">
+          <Form.Check
+            type="checkbox"
+            label=" "
+            checked={translation.translated}
+            onClick={() => completeTranslation(translation._id)}
+            readOnly
+          />
+          <Link to={`/${translation._id}`} style={{ textDecoration: 'none' }}>
+            {translation.name}
+          </Link>
+        </div>  
+        <Badge>{translation.numberOfPages} pages</Badge>
+      </ListGroup.Item>
     </div>
   );
 };
