4ffe714f7393f0eda851802ebf26af597e64e2a1
adriandamii <37124920+adriandamii@users.noreply.github.com>
2022-08-22 10:25:41 +0300

    Details translation changed

--- client/src/components/Translation.js
+++ client/src/components/Translation.js
@@ -5,7 +5,7 @@ import {
   ListGroup,
   Modal,
   OverlayTrigger,
-  Popover,
+  Tooltip,
 } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -15,16 +15,16 @@ import toast from 'react-hot-toast';
 const deleteToast = () => toast.success('Successfully deleted!');
 
 const Translation = (props) => {
+  const [checkPress, setCheckPress] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
+  const [showDetail, setShowDetail] = useState(false);
   const [deleteItem, setDeleteItem] = useState(false);
-  const [show, setShow] = useState(false);
-  const [detailButton, setDetailButton] = useState(false);
+  const [aux, setAux] = useState(false);
+  
   const { translation, setTranslations } = props;
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  
-  const handleClose = () => {
-    setShow(false);
-  };
+  let classString = 'fas fa-trash deleteButton';
   
   useEffect(() => {
     if (deleteItem === true) {
@@ -35,6 +35,7 @@ const Translation = (props) => {
   }, [deleteItem]);
   
   const completeTranslation = async (id) => {
+    setCheckPress(true);
     const data = await fetch(
       `http://localhost:5000/translations/complete/${id}`
     ).then((res) => res.json());
@@ -49,97 +50,118 @@ const Translation = (props) => {
     );
   };
   
-  const deleteHandler = () => {
-    setShow(true);
+  useEffect(() => {
+    if (showDetail === true && showDelete === false && aux === true) {
+      setShowDetail(false);
+      setAux(false);
+    } else if (showDetail === true && showDelete === true) {
+      setShowDetail(false);
+      setAux(true);
+    }
+    if (showDetail === true && checkPress === true) {
+      setShowDetail(false);
+      setCheckPress(false);
+    }
+  }, [showDetail, showDelete, aux]);
+  
+  const handleClose = () => {
+    setShowDelete(false);
   };
   
-  const popover = (
-    <Popover id="popover-basic">
-      <Popover.Header as="h3" className="pop">
-        Details
-        <Button
-          className="edit-button"
-          size="sm"
-          variant="secondary"
-          onClick={() => navigate(`/${translation._id}/edit`)}
-        >
-          <i className="fas fa-pen"></i>
-        </Button>
-      </Popover.Header>
-      <Popover.Body>
-        <strong>Name:</strong> {translation.name}
-        <br></br>
-        <strong>Office:</strong> {translation.office}
-        <br></br>
-        <strong>Pages:</strong> {translation.numberOfPages}
-        <br></br>
-        <strong>Price Page:</strong> {translation.pagePrice}
-        <br></br>
-        <strong>Created:</strong> {translation.date}
-        <br></br>
-      </Popover.Body>
-    </Popover>
+  const hclose = () => {
+    setShowDetail(false);
+  };
+  
+  const deleteHandler = () => {
+    setShowDelete(true);
+  };
+  
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Translation status
+    </Tooltip>
   );
   
   return (
-    <ListGroup.Item
-      key={translation._id}
-      action
-      className="list-item"
-      variant={translation.translated === true ? 'success' : 'danger'}
-    >
-      <div className="d-flex">
-        <Form.Check
-          type="checkbox"
-          label=" "
-          checked={translation.translated}
-          onClick={() => completeTranslation(translation._id)}
-          readOnly
-        />
-        {translation.name}
-      </div>
-      <div>
-        <Button
-          size="sm"
-          variant="danger"
-          type="button"
-          className="deleteButton"
-          onClick={() => deleteHandler(translation)}
-        >
-          <i className="fas fa-trash"></i>
-        </Button>{' '}
-        <OverlayTrigger trigger="click" placement="right" overlay={popover}>
-          <Button
-            size="sm"
-            type="button"
-            className="detail-button"
-            variant="success"
-            onClick={() => setDetailButton(!detailButton)}
-          >
-            {detailButton ? 'Hide details' : 'See details'}
-          </Button>
-        </OverlayTrigger>
-      </div> 
+    <>
       <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
+        show={showDetail}
+        size="sm"
+        onHide={hclose}
+        aria-labelledby="example-modal-sizes-title-sm"
       >
         <Modal.Header closeButton>
-          <Modal.Title>{translation.name}</Modal.Title>
+          <Modal.Title id="example-modal-sizes-title-sm">
+            Details{' '}
+            <i
+              className="fas fa-pen"
+              onClick={() => navigate(`/${translation._id}/edit`)}
+            ></i>
+          </Modal.Title>
         </Modal.Header>
-        <Modal.Body>Are you sure you want to delete?</Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={() => setDeleteItem(true)}>
-            Yes
-          </Button>
-          <Button variant="secondary" onClick={handleClose}>
-            No
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+          <strong>Name:</strong> {translation.name}
+          <br></br>
+          <strong>Office:</strong> {translation.office}
+          <br></br>
+          <strong>Pages:</strong> {translation.numberOfPages}
+          <br></br>
+          <strong>Price Page:</strong> {translation.pagePrice}
+          <br></br>
+          <strong>Created:</strong> {translation.date}
+          <br></br>
+        </Modal.Body>
       </Modal>
-    </ListGroup.Item>
+      <ListGroup.Item
+        key={translation._id}
+        action
+        className="list-item"
+        variant={translation.translated === true ? 'success' : 'danger'}
+        onClick={() => setShowDetail(true)}
+      >
+        <div className="d-flex">
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip}
+          >
+            <Form.Check
+              type="checkbox"
+              label=" "
+              checked={translation.translated}
+              onClick={() => completeTranslation(translation._id)}
+              readOnly
+            />
+          </OverlayTrigger>
+          {translation.name}
+        </div> 
+        <div>
+          <i
+            onClick={() => deleteHandler(translation)}
+            className={`${classString}`}
+          ></i>
+        </div>
+        <Modal
+          show={showDelete}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>{translation.name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Are you sure you want to delete?</Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={() => setDeleteItem(true)}>
+              Yes
+            </Button>
+            <Button variant="secondary" onClick={handleClose}>
+              No
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </ListGroup.Item>
+    </>
   );
 };
 
