e9bfda5816244e13646f3c9ff58c1cb588e6b330
adriandamii <37124920+adriandamii@users.noreply.github.com>
2022-09-09 21:28:44 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,25 +1,32 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import translationsRoutes from './routes/translation.js';
 
-const app = express();
+dotenv.config();
 
-app.use(express.json({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-app.use('/translations', translationsRoutes);
-
-const CONNECTION_URL = '';
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
     )
   )
   .catch((error) => console.log(`${error} failed to connect`));
+
+const app = express();
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use('/translations', translationsRoutes);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
+);
