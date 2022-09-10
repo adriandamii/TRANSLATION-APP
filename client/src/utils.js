40dd43dbef8a9a092402d55d0c1e8617ba1e35db
adriandamii <37124920+adriandamii@users.noreply.github.com>
2022-09-10 12:30:20 +0300

    Months objects added

---
+++ client/src/utils.js
@@ -0,0 +1,50 @@
+export const months = [
+  {
+    name: 'January',
+    nr: 1,
+  },
+  {
+    name: `February`,
+    nr: 2,
+  },
+  {
+    name: `March`,
+    nr: 3,
+  },
+  {
+    name: `April`,
+    nr: 4,
+  },
+  {
+    name: 'May',
+    nr: 5,
+  },
+  {
+    name: `June`,
+    nr: 6,
+  },
+  {
+    name: `July`,
+    nr: 7,
+  },
+  {
+    name: `August`,
+    nr: 8,
+  },
+  {
+    name: 'September',
+    nr: 9,
+  },
+  {
+    name: `Octomber`,
+    nr: 10,
+  },
+  {
+    name: `November`,
+    nr: 11,
+  },
+  {
+    name: `December`,
+    nr: 12,
+  },
+];
